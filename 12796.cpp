a48cdf2492e3d2295548c74bdcee8b1e3f120d02
dmstkd7 <eeunsang7@naver.com>
2016-06-01 00:30:11 +0900

    나의 행렬곱셈 답사기
    복잡해 보이지만 생각만 한다면 코딩이 매우 단순히 질수 있었음

---
+++ 12796.cpp
@@ -0,0 +1,14 @@
+//#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+using namespace std;
+ 
+ 
+int main(void){
+ 
+    int N;
+    scanf("%d", &N);
+    printf("3\n");
+    printf("%d 1 1 1", N + 1);
+ 
+    return 0;
+}
